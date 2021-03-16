9911c45830128064d8b0289bdd01296d99420ca9
Rafael Ponte <rafael.ponte@zup.com.br>
2021-03-16 18:36:32 -0300

    [testes] testes de integração para ListaChavesPixEndpoint;

---
+++ src/test/kotlin/br/com/zup/edu/pix/lista/ListaChavesEndpointTest.kt
@@ -0,0 +1,124 @@
+package br.com.zup.edu.pix.lista
+
+import br.com.zup.edu.grpc.KeymanagerListaGrpcServiceGrpc
+import br.com.zup.edu.grpc.ListaChavesPixRequest
+import br.com.zup.edu.pix.*
+import io.grpc.ManagedChannel
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.micronaut.context.annotation.Bean
+import io.micronaut.context.annotation.Factory
+import io.micronaut.grpc.annotation.GrpcChannel
+import io.micronaut.grpc.server.GrpcServerChannel
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
+
+@MicronautTest
+internal class ListaChavesEndpointTest(
+    val repository: ChavePixRepository,
+    val grpcClient: KeymanagerListaGrpcServiceGrpc.KeymanagerListaGrpcServiceBlockingStub,
+) {
+
+    companion object {
+        val CLIENTE_ID = UUID.randomUUID()
+    }
+
+    @BeforeEach
+    fun setup() {
+        repository.save(chave(tipo = TipoDeChave.EMAIL, chave = "rafael.ponte@zup.com.br", clienteId = CLIENTE_ID))
+        repository.save(chave(tipo = TipoDeChave.ALEATORIA, chave = "randomkey-2", clienteId = UUID.randomUUID()))
+        repository.save(chave(tipo = TipoDeChave.ALEATORIA, chave = "randomkey-3", clienteId = CLIENTE_ID))
+    }
+
+    @AfterEach
+    fun cleanUp() {
+        repository.deleteAll()
+    }
+
+    @Test
+    fun `deve listar todas as chaves do cliente`() {
+        // cenário
+        val clienteId = CLIENTE_ID.toString()
+        // FIXME: inserir chaves aqui não funciona, ao que tudo indica o repository usa uma conexão diferente, mesmo com @Transactional
+
+        // ação
+        val response = grpcClient.lista(ListaChavesPixRequest.newBuilder()
+                                                        .setClienteId(clienteId)
+                                                        .build())
+
+        // validação
+        with (response.chavesList) {
+            assertEquals(2, size)
+            this.map { Pair(it.tipo, it.chave) }
+                .containsAll(listOf(
+                    Pair(br.com.zup.edu.grpc.TipoDeChave.ALEATORIA, "randomkey-3"),
+                    Pair(br.com.zup.edu.grpc.TipoDeChave.EMAIL, "rafael.ponte@zup.com.br")
+                ))
+        }
+    }
+
+    @Test
+    fun `nao deve listar as chaves do cliente quando cliente nao possuir chaves`() {
+        // cenário
+        val clienteSemChaves = UUID.randomUUID().toString()
+
+        // ação
+        val response = grpcClient.lista(ListaChavesPixRequest.newBuilder()
+            .setClienteId(clienteSemChaves)
+            .build())
+
+        // validação
+        assertEquals(0, response.chavesCount)
+    }
+
+    @Test
+    fun `nao deve listar todas as chaves do cliente quando clientId for invalido`() {
+        // cenário
+        val clienteIdInvalido = ""
+
+        // ação
+        val thrown = org.junit.jupiter.api.assertThrows<StatusRuntimeException> {
+            val response = grpcClient.lista(ListaChavesPixRequest.newBuilder()
+                .setClienteId(clienteIdInvalido)
+                .build())
+        }
+
+        // validação
+        with (thrown) {
+            assertEquals(Status.INVALID_ARGUMENT.code, status.code)
+            assertEquals("Cliente ID não pode ser nulo ou vazio", status.description)
+        }
+    }
+
+    @Factory
+    class Clients  {
+        @Bean
+        fun blockingStub(@GrpcChannel(GrpcServerChannel.NAME) channel: ManagedChannel): KeymanagerListaGrpcServiceGrpc.KeymanagerListaGrpcServiceBlockingStub? {
+            return KeymanagerListaGrpcServiceGrpc.newBlockingStub(channel)
+        }
+    }
+
+    private fun chave(
+        tipo: TipoDeChave,
+        chave: String = UUID.randomUUID().toString(),
+        clienteId: UUID = UUID.randomUUID(),
+    ): ChavePix {
+        return ChavePix(
+            clienteId = clienteId,
+            tipo = tipo,
+            chave = chave,
+            tipoDeConta = TipoDeConta.CONTA_CORRENTE,
+            conta = ContaAssociada(
+                instituicao = "UNIBANCO ITAU",
+                nomeDoTitular = "Rafael Ponte",
+                cpfDoTitular = "12345678900",
+                agencia = "1218",
+                numeroDaConta = "123456"
+            )
+        )
+    }
+}

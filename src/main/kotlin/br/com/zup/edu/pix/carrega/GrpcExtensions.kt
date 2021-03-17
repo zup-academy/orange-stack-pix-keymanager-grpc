23c9a465636e4217cc4b0eb591aeca2c8e2a036a
Rafael Ponte <rafael.ponte@zup.com.br>
2021-03-17 18:42:13 -0300

    [testes] evita warning de nullidade do kotlin;

--- src/main/kotlin/br/com/zup/edu/pix/carrega/GrpcExtensions.kt
+++ src/main/kotlin/br/com/zup/edu/pix/carrega/GrpcExtensions.kt
@@ -8,7 +8,7 @@ import javax.validation.Validator
 
 fun CarregaChavePixRequest.toModel(validator: Validator): Filtro { // 1
 
-    val filtro = when(filtroCase) { // 1
+    val filtro = when(filtroCase!!) { // 1
         PIXID -> pixId.let { // 1
             Filtro.PorPixId(clienteId = it.clienteId, pixId = it.pixId) // 1
         }

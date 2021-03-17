ba3f93abe09ebce39da46978dbff419bea791e01
Rafael Ponte <rafael.ponte@zup.com.br>
2021-03-17 15:09:47 -0300

    [testes] testes de integração para CarregaChaveEndpoint;

---
+++ src/test/kotlin/br/com/zup/edu/pix/carrega/CarregaChaveEndpointTest.kt
@@ -0,0 +1,195 @@
+package br.com.zup.edu.pix.carrega
+
+import br.com.zup.edu.grpc.CarregaChavePixRequest
+import br.com.zup.edu.grpc.CarregaChavePixRequest.FiltroPorPixId
+import br.com.zup.edu.grpc.KeymanagerCarregaGrpcServiceGrpc
+import br.com.zup.edu.integration.bcb.*
+import br.com.zup.edu.pix.*
+import br.com.zup.edu.pix.TipoDeChave.*
+import io.grpc.ManagedChannel
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.micronaut.context.annotation.Bean
+import io.micronaut.context.annotation.Factory
+import io.micronaut.grpc.annotation.GrpcChannel
+import io.micronaut.grpc.server.GrpcServerChannel
+import io.micronaut.http.HttpResponse
+import io.micronaut.test.annotation.MockBean
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import junit.framework.Assert.assertEquals
+import org.junit.jupiter.api.*
+import org.mockito.Mockito
+import java.time.LocalDateTime
+import java.util.*
+import javax.inject.Inject
+
+@MicronautTest(transactional = false)
+internal class CarregaChaveEndpointTest(
+    val repository: ChavePixRepository,
+    val grpcClient: KeymanagerCarregaGrpcServiceGrpc.KeymanagerCarregaGrpcServiceBlockingStub,
+) {
+
+    @Inject
+    lateinit var bcbClient: BancoCentralClient
+
+    companion object {
+        val CLIENTE_ID = UUID.randomUUID()
+    }
+
+    /**
+     * TIP: por padrão roda numa transação isolada
+     */
+    @BeforeEach
+    fun setup() {
+        repository.save(chave(tipo = EMAIL, chave = "rafael.ponte@zup.com.br", clienteId = CLIENTE_ID))
+        repository.save(chave(tipo = CPF, chave = "63657520325", clienteId = UUID.randomUUID()))
+        repository.save(chave(tipo = ALEATORIA, chave = "randomkey-3", clienteId = CLIENTE_ID))
+        repository.save(chave(tipo = CELULAR, chave = "+551155554321", clienteId = CLIENTE_ID))
+    }
+
+    /**
+     * TIP: por padrão roda numa transação isolada
+     */
+    @AfterEach
+    fun cleanUp() {
+        repository.deleteAll()
+    }
+
+    @Test
+    fun `deve carregar chave por pixId e clienteId`() {
+        // cenário
+        val chaveExistente = repository.findByChave("+551155554321").get()
+
+        // ação
+        val response = grpcClient.carrega(CarregaChavePixRequest.newBuilder()
+                                                                .setPixId(FiltroPorPixId.newBuilder()
+                                                                    .setPixId(chaveExistente.id.toString())
+                                                                    .setClienteId(chaveExistente.clienteId.toString())
+                                                                    .build()
+                                                                ).build())
+
+        // validação
+        with(response) {
+            assertEquals(chaveExistente.id.toString(), this.pixId)
+            assertEquals(chaveExistente.clienteId.toString(), this.clienteId)
+            assertEquals(chaveExistente.tipo.name, this.chave.tipo.name)
+            assertEquals(chaveExistente.chave, this.chave.chave)
+        }
+    }
+
+    @Test
+    fun `deve carregar chave por valor da chave quando registro existente localmente`() {
+        // cenário
+        val chaveExistente = repository.findByChave("rafael.ponte@zup.com.br").get()
+
+        // ação
+        val response = grpcClient.carrega(CarregaChavePixRequest.newBuilder()
+                                            .setChave("rafael.ponte@zup.com.br")
+                                            .build())
+
+        // validação
+        with(response) {
+            assertEquals(chaveExistente.id.toString(), this.pixId)
+            assertEquals(chaveExistente.clienteId.toString(), this.clienteId)
+            assertEquals(chaveExistente.tipo.name, this.chave.tipo.name)
+            assertEquals(chaveExistente.chave, this.chave.chave)
+        }
+    }
+
+    @Test
+    fun `deve carregar chave por valor da chave quando registro nao existir localmente`() {
+        // cenário
+        val bcbResponse = pixKeyDetailsResponse()
+        Mockito.`when`(bcbClient.findByKey(key = "user.from.another.bank@santander.com.br"))
+            .thenReturn(HttpResponse.ok(pixKeyDetailsResponse()))
+
+        // ação
+        val response = grpcClient.carrega(CarregaChavePixRequest.newBuilder()
+                                    .setChave(bcbResponse.key)
+                                    .build())
+
+        // validação
+        with(response) {
+            assertEquals("", this.pixId)
+            assertEquals("", this.clienteId)
+            assertEquals(bcbResponse.keyType.name, this.chave.tipo.name)
+            assertEquals(bcbResponse.key, this.chave.chave)
+        }
+    }
+
+    @Test
+    fun `nao deve carregar chave quando filtro invalido`() {
+
+        // ação
+        val thrown = assertThrows<StatusRuntimeException> {
+            grpcClient.carrega(CarregaChavePixRequest.newBuilder().build())
+        }
+
+        // validação
+        with(thrown) {
+            Assertions.assertEquals(Status.INVALID_ARGUMENT.code, status.code)
+            Assertions.assertEquals("Chave Pix inválida ou não informada", status.description)
+        }
+    }
+
+    private fun chave(
+        tipo: TipoDeChave,
+        chave: String = UUID.randomUUID().toString(),
+        clienteId: UUID = UUID.randomUUID(),
+    ): ChavePix {
+        return ChavePix(
+            clienteId = clienteId,
+            tipo = tipo,
+            chave = chave,
+            tipoDeConta = TipoDeConta.CONTA_CORRENTE,
+            conta = ContaAssociada(
+                instituicao = "UNIBANCO ITAU",
+                nomeDoTitular = "Rafael Ponte",
+                cpfDoTitular = "12345678900",
+                agencia = "1218",
+                numeroDaConta = "123456"
+            )
+        )
+    }
+
+    private fun pixKeyDetailsResponse(): PixKeyDetailsResponse {
+        return PixKeyDetailsResponse(
+            keyType = PixKeyType.EMAIL,
+            key = "user.from.another.bank@santander.com.br",
+            bankAccount = bankAccount(),
+            owner = owner(),
+            createdAt = LocalDateTime.now()
+        )
+    }
+
+    private fun bankAccount(): BankAccount {
+        return BankAccount(
+            participant = "90400888",
+            branch = "9871",
+            accountNumber = "987654",
+            accountType = BankAccount.AccountType.SVGS
+        )
+    }
+
+    private fun owner(): Owner {
+        return Owner(
+            type = Owner.OwnerType.NATURAL_PERSON,
+            name = "Another User",
+            taxIdNumber = "12345678901"
+        )
+    }
+
+    @MockBean(BancoCentralClient::class)
+    fun bcbClient(): BancoCentralClient? {
+        return Mockito.mock(BancoCentralClient::class.java)
+    }
+
+    @Factory
+    class Clients  {
+        @Bean
+        fun blockingStub(@GrpcChannel(GrpcServerChannel.NAME) channel: ManagedChannel): KeymanagerCarregaGrpcServiceGrpc.KeymanagerCarregaGrpcServiceBlockingStub? {
+            return KeymanagerCarregaGrpcServiceGrpc.newBlockingStub(channel)
+        }
+    }
+
+}

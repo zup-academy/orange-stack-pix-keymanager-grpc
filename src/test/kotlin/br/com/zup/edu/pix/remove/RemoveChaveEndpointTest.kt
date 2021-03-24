9bffd2e78cfa7aae7941914f6673498c563138d4
Rafael Ponte <rafael.ponte@zup.com.br>
2021-03-24 11:03:50 -0300

    [testes] move cenários de testes da classe RemoveChaveServiceTest para RemoveChaveEndpointTest;

--- src/test/kotlin/br/com/zup/edu/pix/remove/RemoveChaveEndpointTest.kt
+++ src/test/kotlin/br/com/zup/edu/pix/remove/RemoveChaveEndpointTest.kt
@@ -2,7 +2,10 @@ package br.com.zup.edu.pix.remove
 
 import br.com.zup.edu.grpc.KeymanagerRemoveGrpcServiceGrpc
 import br.com.zup.edu.grpc.RemoveChavePixRequest
-import br.com.zup.edu.pix.ChavePixNaoEncontradaException
+import br.com.zup.edu.integration.bcb.BancoCentralClient
+import br.com.zup.edu.integration.bcb.DeletePixKeyRequest
+import br.com.zup.edu.integration.bcb.DeletePixKeyResponse
+import br.com.zup.edu.pix.*
 import io.grpc.ManagedChannel
 import io.grpc.Status
 import io.grpc.StatusRuntimeException
@@ -10,73 +13,130 @@ import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Factory
 import io.micronaut.grpc.annotation.GrpcChannel
 import io.micronaut.grpc.server.GrpcServerChannel
+import io.micronaut.http.HttpResponse
 import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.mockito.Mockito.*
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import java.time.LocalDateTime
 import java.util.*
 import javax.inject.Inject
 
 @MicronautTest
-internal class RemoveChaveEndpointTest {
+internal class RemoveChaveEndpointTest(
+    val repository: ChavePixRepository,
+    val grpcClient: KeymanagerRemoveGrpcServiceGrpc.KeymanagerRemoveGrpcServiceBlockingStub,
+) {
 
     @Inject
-    lateinit var grpcClient: KeymanagerRemoveGrpcServiceGrpc.KeymanagerRemoveGrpcServiceBlockingStub
+    lateinit var bcbClient: BancoCentralClient
 
-    @Inject
-    lateinit var service: RemoveChaveService
+    lateinit var CHAVE_EXISTENTE: ChavePix
+
+    @BeforeEach
+    fun setup() {
+        CHAVE_EXISTENTE = repository.save(chave(
+            tipo = TipoDeChave.EMAIL,
+            chave = "rponte@gmail.com",
+            clienteId = UUID.randomUUID()
+        ))
+    }
+
+    @AfterEach
+    fun cleanUp() {
+        repository.deleteAll()
+    }
 
     @Test
-    fun `deve remover chave Pix`() {
+    fun `deve remover chave pix existente`() {
         // cenário
-        val clienteId = UUID.randomUUID().toString()
-        val pixId = UUID.randomUUID().toString()
-
-        doNothing().`when`(service).remove(clienteId = clienteId, pixId = pixId)
+        `when`(bcbClient.delete("rponte@gmail.com", DeletePixKeyRequest("rponte@gmail.com")))
+            .thenReturn(HttpResponse.ok(DeletePixKeyResponse(key = "rponte@gmail.com",
+                participant = ContaAssociada.ITAU_UNIBANCO_ISPB,
+                deletedAt = LocalDateTime.now()))
+            )
 
         // ação
         val response = grpcClient.remove(RemoveChavePixRequest.newBuilder()
-                                                    .setPixId(pixId)
-                                                    .setClienteId(clienteId)
+                                                    .setPixId(CHAVE_EXISTENTE.id.toString())
+                                                    .setClienteId(CHAVE_EXISTENTE.clienteId.toString())
                                                     .build())
 
         // validação
-        assertEquals(pixId, response.pixId)
-        assertEquals(clienteId, response.clienteId)
+        assertEquals(CHAVE_EXISTENTE.id.toString(), response.pixId)
+        assertEquals(CHAVE_EXISTENTE.clienteId.toString(), response.clienteId)
     }
 
-    /**
-     * XXX: vale a pena ter dado que não existe branches no código?
-     */
     @Test
-    fun `nao deve remover chave Pix quando chave nao encontrada`() {
+    fun `nao deve remover chave pix existente quando ocorrer algum erro no serviço do BCB`() {
         // cenário
-        val clienteId = UUID.randomUUID().toString()
-        val pixId = UUID.randomUUID().toString()
-
-        `when`(service.remove(clienteId = clienteId, pixId = pixId))
-            .thenThrow(ChavePixNaoEncontradaException("Chave Pix não encontrada"))
+        `when`(bcbClient.delete("rponte@gmail.com", DeletePixKeyRequest("rponte@gmail.com")))
+            .thenReturn(HttpResponse.unprocessableEntity())
 
         // ação
         val thrown = assertThrows<StatusRuntimeException> {
             grpcClient.remove(RemoveChavePixRequest.newBuilder()
-                                        .setPixId(pixId)
-                                        .setClienteId(clienteId)
-                                        .build())
+                                    .setPixId(CHAVE_EXISTENTE.id.toString())
+                                    .setClienteId(CHAVE_EXISTENTE.clienteId.toString())
+                                    .build())
         }
 
         // validação
-        with (thrown) {
-            assertEquals(Status.NOT_FOUND.code, status.code)
-            assertEquals("Chave Pix não encontrada", status.description)
+        with(thrown) {
+            assertEquals(Status.FAILED_PRECONDITION.code, status.code)
+            assertEquals("Erro ao remover chave Pix no Banco Central do Brasil (BCB)", status.description)
         }
     }
 
-    @MockBean(RemoveChaveService::class)
-    fun mockedService(): RemoveChaveService {
-        return mock(RemoveChaveService::class.java)
+    @Test
+    fun `nao deve remover chave pix quando chave inexistente`() {
+        // cenário
+        val pixIdNaoExistente = UUID.randomUUID().toString()
+
+        // ação
+        val thrown = assertThrows<StatusRuntimeException> {
+            grpcClient.remove(RemoveChavePixRequest.newBuilder()
+                                    .setPixId(pixIdNaoExistente)
+                                    .setClienteId(CHAVE_EXISTENTE.clienteId.toString())
+                                    .build())
+        }
+
+        with(thrown) {
+            assertEquals(Status.NOT_FOUND.code, status.code)
+            assertEquals("Chave Pix não encontrada ou não pertence ao cliente", status.description)
+        }
+    }
+
+    /**
+     * XXX: será que precisamos disso dado que não existe branch explicito no códgigo, mas sim na query?
+     */
+    @Test
+    fun `nao deve remover chave pix quando chave existente mas pertence a outro cliente`() {
+        // cenário
+        val outroClienteId = UUID.randomUUID().toString()
+
+        // ação
+        val thrown = assertThrows<StatusRuntimeException> {
+            grpcClient.remove(RemoveChavePixRequest.newBuilder()
+                                    .setPixId(CHAVE_EXISTENTE.id.toString())
+                                    .setClienteId(outroClienteId)
+                                    .build())
+        }
+
+        with(thrown) {
+            assertEquals(Status.NOT_FOUND.code, status.code)
+            assertEquals("Chave Pix não encontrada ou não pertence ao cliente", status.description)
+        }
+    }
+
+    @MockBean(BancoCentralClient::class)
+    fun bcbClient(): BancoCentralClient? {
+        return mock(BancoCentralClient::class.java)
     }
 
     @Factory
@@ -85,5 +145,25 @@ internal class RemoveChaveEndpointTest {
         fun blockingStub(@GrpcChannel(GrpcServerChannel.NAME) channel: ManagedChannel): KeymanagerRemoveGrpcServiceGrpc.KeymanagerRemoveGrpcServiceBlockingStub? {
             return KeymanagerRemoveGrpcServiceGrpc.newBlockingStub(channel)
         }
+    }
+
+    private fun chave(
+        tipo: TipoDeChave,
+        chave: String = UUID.randomUUID().toString(),
+        clienteId: UUID = UUID.randomUUID(),
+    ): ChavePix {
+        return ChavePix(
+            clienteId = clienteId,
+            tipo = tipo,
+            chave = chave,
+            tipoDeConta = TipoDeConta.CONTA_CORRENTE,
+            conta = ContaAssociada(
+                instituicao = "UNIBANCO ITAU",
+                nomeDoTitular = "Rafael Ponte",
+                cpfDoTitular = "12345678900",
+                agencia = "1218",
+                numeroDaConta = "123456"
+            )
+        )
     }
 }
